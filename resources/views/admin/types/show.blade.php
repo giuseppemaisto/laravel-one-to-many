5a1075d1ecc94f00a5411322123094e5a1b6d4f5
giuseppemaisto <gmaisto98@gmail.com>
2023-03-05 20:04:25 +0100

    bonus 3 completo: esercizio finito

--- resources/views/admin/types/show.blade.php
+++ resources/views/admin/types/show.blade.php
@@ -15,10 +15,27 @@
             <div class="col-12">
                <p><strong>slug</strong>: {{$type->slug}}</p>
             </div>
-            <div class="col-12">
+            <div class="col-12 ">
                 <h2>progetti appartenenti a questa categoria </h2>
                 {{count($type->posts)}}
+                <hr>
+                <div class="row mt-5 d-flex ">
+                    @forelse($type->posts as $post)
+                    <div class="col-4 py-2">
+                        <div class="card p-3">
+                            <h5>{{ $post->title}}</h5>
+                            <p>{{$post->description}}</p>
+                            <a class="btn btn-primary" href="{{ route('admin.posts.show', $post->slug)}}"> continua a leggere</a>
+                        </div>
+                    </div>
+                    @empty
+                    <h2>non ci sono progetti in questa categoria</h2>
+                    @endforelse
+                </div>
             </div>
+
+            
         </div>
     </div>
+
 @endsection
